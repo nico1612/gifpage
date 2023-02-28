e41ab8fe21a7eaf9de8d140f4a06df9e6a569256
nico1612 <nicogonzalezraggi123@hotmail.com>
2023-02-28 10:24:55 -0300

    prop-types

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,10 +1,15 @@
+import PropTypes from 'prop-types'
 
-
-export const GiftItem=({title,url})=>{
+export const GiftItem=({title,url,id})=>{
     return(
         <div className="card">
             <img src={url} alt={url}/>
             <p>{title}</p>
         </div>
     )
+}
+
+CounterApp.propTypes={
+    title:PropTypes.isRequired,
+    url:PropTypes.isRequired
 }
